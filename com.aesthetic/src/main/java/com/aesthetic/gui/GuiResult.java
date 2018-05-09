f5d284e0dca7e441ff091aed9802a38ac90aefab
tschne16 <tschne16@smail.uni-koeln.de>
2018-05-09 22:09:43 +0200

    GUI RESULT

--- com.aesthetic/src/main/java/com/aesthetic/gui/GuiResult.java
+++ com.aesthetic/src/main/java/com/aesthetic/gui/GuiResult.java
@@ -14,23 +14,33 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
 
 public class GuiResult extends JFrame {
 public HashMap<String,String> all;	
 public int counter = 0;
 public Iterator it;
+JLabel lblPic;
 private JLabel lblPicture;
 	public GuiResult(HashMap<String,String>  results) throws IOException {
 		getContentPane().setLayout(null);
 		
 		all = results;
 		JLabel lblName = new JLabel("Name");
-		lblName.setBounds(10, 344, 414, 14);
+		lblName.setBounds(79, 344, 345, 14);
 		getContentPane().add(lblName);
 		
 		JLabel lblKlasse = new JLabel("Klasse");
-		lblKlasse.setBounds(10, 373, 296, 14);
+		lblKlasse.setBounds(79, 373, 200, 14);
 		getContentPane().add(lblKlasse);
+		
+		
+         lblPic = new JLabel("");
+        lblPic.setBounds(289, 373, 46, 14);
+        getContentPane().add(lblPic);
+        ImageIcon image = new ImageIcon(GuiResult.class.getResource("res/check.png"));
+        lblPic.setVisible(false);
+		lblPic.setIcon(image);
 		
 		boolean visible = false;
 		if(results.size()>1)
@@ -43,6 +53,17 @@ private JLabel lblPicture;
 
 		        Map.Entry pair = (Map.Entry)it.next(); 
 		        lblName.setText(pair.getKey().toString());
+		        erg = new File(pair.getKey().toString()).getParentFile().getName();
+		        
+    			if(erg.toUpperCase().equals(pair.getValue().toString().toUpperCase()))
+    			{
+    				lblPic.setVisible(true);
+    			}
+    			else
+    			{
+    				lblPic.setVisible(false);
+    			}
+    			
 		        BufferedImage myPicture = ImageIO.read(new File(pair.getKey().toString()));
 		        lblKlasse.setText(pair.getValue().toString());
 		        if(myPicture != null )
@@ -64,8 +85,19 @@ private JLabel lblPicture;
 		        		}
 		        			Map.Entry pair = (Map.Entry)it.next();
 		        			lblName.setText(pair.getKey().toString());
+		        			String erg = new File(pair.getKey().toString()).getParentFile().getName();
 		        			lblKlasse.setText(pair.getValue().toString());
-		        			  try {
+		        			  
+		        			if(erg.toUpperCase().equals(pair.getValue().toString().toUpperCase()))
+		        			{
+		        				lblPic.setVisible(true);
+		        			}
+		        			else
+		        			{
+		        				lblPic.setVisible(false);
+		        			}
+		        			
+		        			try {
 		        				  BufferedImage	myPicture = ImageIO.read(new File(pair.getKey().toString()));
 		        				  lblPicture.setIcon(new ImageIcon(myPicture));
 		        			  } catch (IOException e) {
@@ -86,6 +118,16 @@ private JLabel lblPicture;
 		        });
 		        btnNext.setBounds(335, 369, 89, 23);
 		        getContentPane().add(btnNext);
+		        
+		        JLabel lblPrediction = new JLabel("Prediction:");
+		        lblPrediction.setBounds(10, 373, 59, 14);
+		        getContentPane().add(lblPrediction);
+		        
+		        JLabel lblPath = new JLabel("Path");
+		        lblPath.setBounds(10, 344, 59, 14);
+		        getContentPane().add(lblPath);
+		        
+
 		      
 
 		        
@@ -96,5 +138,4 @@ private JLabel lblPicture;
 		
 		
 	}
-
 }
